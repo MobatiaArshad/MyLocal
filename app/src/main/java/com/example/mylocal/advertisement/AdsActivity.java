1d547f6d66ed86d541b55253fec03f618323012f
Arshad <muhammed@revyrieglobal.com>
2022-12-06 12:00:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mylocal/advertisement/AdsActivity.java
@@ -0,0 +1,16 @@
+package com.example.mylocal.advertisement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mylocal.R;
+
+public class AdsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ads);
+    }
+}
