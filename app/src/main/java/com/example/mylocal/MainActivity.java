57326655a05cd9b394e568a5353e8b9d4babbc18
Arshad <mohdard@gmail.com>
2022-12-26 18:01:23 +0530

    - Controlled heap level - Transport completed

--- app/src/main/java/com/example/mylocal/MainActivity.java
+++ app/src/main/java/com/example/mylocal/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
 import com.example.mylocal.advertisement.AdsActivity;
 import com.example.mylocal.news.NewsActivity;
 import com.example.mylocal.touristGuide.TouristGuideActivity;
@@ -15,7 +17,8 @@ import com.example.mylocal.transport.TransportActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayoutCompat ads,guide,transport,news;
+    LinearLayoutCompat ads, guide, transport, news;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
         news = findViewById(R.id.newsBtn);
         guide = findViewById(R.id.guideBtn);
         transport = findViewById(R.id.transportBtn);
+        imageView = findViewById(R.id.greenWitchImage);
+
+        Glide.with(this)
+                .asBitmap()
+                .load("https://www.visitgreenwich.org.uk/imageresizer/?image=%2Fdbimgs%2FGRN%20-%20Media%20Gallery%20-%20Ideas%20%26%20Inspo%20-%20WHS.png&action=MediaGallery")
+                .into(imageView);
+
 
         ads.setOnClickListener(v -> startActivity(new Intent(this, AdsActivity.class)));
 
